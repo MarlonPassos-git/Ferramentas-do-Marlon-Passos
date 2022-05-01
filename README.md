985b1b163039f1fdb5d4df59ed37fff6c6c3be2d
marlon Felipe Passos <marlonpassos@agenciam3.com>
2022-04-30 22:09:45 -0300

    big bang �

---
+++ README.md
@@ -0,0 +1,51 @@
+# Ferramentas do Marlon Passos 
+
+Essa extensão foi desenvolvida por Marlon Passos, e é um acumulador de ferramentas que me ajudam no dia a dia 
+
+## Snippets
+
+---
+- **Atalho:** ``efin``
+- **Descrição:** exporta função com interface com nome
+- **Arquivo:** ``teste.ts`` 
+```tsx
+interface ITest {
+
+}
+
+export function test({
+
+}: ITest) {
+
+}
+```
+---
+- **Atalho:** ``efsn``
+- **Descrição:** exporta função simples com nome
+- **Arquivo:** ``teste.ts`` 
+```tsx
+export function test() {
+
+}
+
+```
+---
+- **Atalho:** ``edfsn``
+- **Descrição:** exporta default função simples com nome
+- **Arquivo:** ``teste.ts`` 
+```tsx
+export default function test() {
+
+}
+
+```
+---
+- **Atalho:** ``fs``
+- **Descrição:** função simples
+- **Arquivo:** ``teste.ts`` 
+```tsx
+function () {
+
+}
+
+```
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+import gulp from "gulp";
+import gulpMergeJson from 'gulp-merge-json';
+const { src, dest, watch } = gulp;
+
+function joinJSONs(extension) {
+  const gulpMergeJsonConfig = {
+    fileName: `snippets.${extension}.code-snippets`,
+  }
+
+  return src([`src/snippets/${extension}/*.json`])
+    .pipe(gulpMergeJson(gulpMergeJsonConfig))
+    .pipe(dest("dist/snippets"));
+}
+
+function joinSchema() {
+  const gulpMergeJsonConfig = {
+    fileName: `schema.json`,
+  }
+
+  return src([`src/schema/*.json`])
+    .pipe(gulpMergeJson(gulpMergeJsonConfig))
+    .pipe(dest("dist/schema"));
+}
+
+
+function time () {
+  const data = new Date();
+  const hora = data.getHours();
+  const minuto = data.getMinutes();
+  const segundo = data.getSeconds();
+  const horaFormatada = hora < 10 ? `0${hora}` : hora;
+  const minutoFormatado = minuto < 10 ? `0${minuto}` : minuto;
+  const segundoFormatado = segundo < 10 ? `0${segundo}` : segundo;
+
+  return `[${horaFormatada}:${minutoFormatado}:${segundoFormatado}]`;
+}
+
+
+async function dev () {
+  await joinJSONs('json');
+  console.log(time() + ' JSON snippets generated');
+  await joinJSONs('css');
+  console.log(time() + ' CSS snippets generated');
+  await joinJSONs('ts');
+  console.log(time() + ' CSS snippets generated');
+  await joinJSONs('js');
+  console.log(time() + ' CSS snippets generated');
+  await joinSchema();
+  console.log(time() + ' JSON schema generated');
+}
+
+const watchOptions = {
+  delay: 150,
+  ignoreInitial: false,
+}
+
+const _dev = dev;
+export { _dev as dev };
+
+watch('src/**/*.json',watchOptions, dev);
